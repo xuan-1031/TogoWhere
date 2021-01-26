6657bd9f1de7276912bd9de487f8f00bd058a694
xuan-1031 <zyx92645@163.com>
2021-01-26 10:17:50 +0800

    项目准备工作

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home"> 
+        <router-link to="/list">列表页</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
