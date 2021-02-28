2ce09949ee6ffdc3b8b20a6613f5874a61041e7f
xuan-1031 <zyx92645@163.com>
2021-02-28 14:01:29 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="home"> 
-        <router-link to="/list">列表页</router-link>
+        <!-- <router-link to="/list">列表页</router-link> -->
+        <HomeHeader></HomeHeader>
     </div>
 </template>
 
 <script>
+    import HomeHeader from './components/Header'
     export default {
-        name: 'Home'
+        name: 'Home',
+        components: {
+            HomeHeader
+        }
     }
 </script>
 
